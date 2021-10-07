77a1103d02607cf0ca7f2c109c5f7bf49a8bd073
Muhammad Wahaj Shafiq <66265270+wahaj04@users.noreply.github.com>
2021-10-07 18:57:57 +0500

    initial commit

--- Martsheet in JavaScript/grade.js
+++ Martsheet in JavaScript/grade.js
@@ -4,18 +4,16 @@ function Total() {
     let subj3 = parseInt(document.getElementById("islam").value);
     let subj4 = parseInt(document.getElementById("math").value);
     let subj5 = parseInt(document.getElementById("phy").value);
-  
+    let student = document.getElementById("student").value;
+
     if (subj1 > 0 && subj1 < 100 && subj2 > 0 && subj2 < 100 && subj3 > 0 && subj3 < 100 && subj4 > 0 && subj4 < 100 && subj5 > 0 && subj5 < 100) {
-        document.write("Your Total Marks" + "<br/>" + "ENGLISH" +subj1 + "<br/>" + "URDU" + subj2 +"<br/>"+"ISLAMIAT"+ subj3 +"<br/>"+ "MATHS" + subj4 +"<br/>"+"PHYSICS" + subj5 + "<br/>");
-
-    let subsum = subj1 + subj2 + subj3 + subj4 + subj5 ;
-    window.alert(subsum);
-    var percentage = (subsum / (500)) * 100;
-        document.write(percentage);
-
-    } 
+        let subsum = subj1 + subj2 + subj3 + subj4 + subj5;
+        alert(subsum);
+        var percentage = (subsum / (500)) * 100;
+        
+        document.getElementById("paragraph").innerHTML = student +"<br/>"+ "YOUR PERCENTAGE THIS" + "<br/>" + percentage;
+    }
     else {
         alert("Your value must be greater than zero")
     };
-    // console.log("hello")
 };
